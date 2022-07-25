5263e270bb0c4e2edcdb1030faa6b7c6427a8c16
mtbbcamp <109309034+mtbbcamp@users.noreply.github.com>
2022-07-25 09:15:46 -0400

    Create caesar_cipher.py
    Stub for caesar cipher

---
+++ caesar_cipher.py
@@ -0,0 +1,22 @@
+
+def cc_encrypt(message, key):
+    encryption = ""
+
+    return encryption
+
+
+def cc_decrypt(encrypted_message, key):
+    decryption = ""
+
+    return decryption
+
+
+def run_tests():
+    cc_decrypt("ALZAPUN", 7)
+    cc_decrypt("JXKAQYXKH ", 23)
+    cc_decrypt("BSUOHWJS", -14)
+    cc_decrypt("HUQBBOBEDWJUNJMYJXRYWDKCRUH", 135762)
+
+
+if __name__ == '__main__':
+    run_tests()
