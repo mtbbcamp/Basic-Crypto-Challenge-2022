0c066d7505b48525482f2c32acd1b99fa4837d73
mtbbcamp <109309034+mtbbcamp@users.noreply.github.com>
2022-07-25 09:18:03 -0400

    Create vigenere_cipher.py
    stub for vigenere cipher

---
+++ vigenere_cipher.py
@@ -0,0 +1,21 @@
+def vc_encrypt(message, key):
+    encryption = ""
+
+    return encryption
+
+
+def vc_decrypt(encrypted_message, key):
+
+    decryption = ""
+
+    return decryption
+
+
+def run_tests():
+    vc_decrypt("CSWMUCWBYHLQXRCSYQQOFQWBPSM ", "INTERN")
+    vc_decrypt("VVJHSMHDLHU", "CAT")
+    vc_decrypt("VQPUXBCLYCOHNXUIBNUBHITCJJAKHCQXSQCPHXYPDAGJZQCFKUSKIFQAR", "BIGBOI")
+
+
+if __name__ == '__main__':
+    run_tests()
